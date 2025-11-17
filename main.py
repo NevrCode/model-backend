379c010ed7c1aaed021748c98f0bdcc8f35a55ad
NevrCode <kevin12keval@gmail.com>
2025-11-17 12:28:34 +0700

    added mqtt and firebase

--- main.py
+++ main.py
@@ -1,8 +1,75 @@
-
+import os
 import flask_cors
 import joblib
 from flask import Flask, request, jsonify
 import numpy as np
+
+import json
+import time
+import threading
+from datetime import datetime
+import paho.mqtt.client as mqtt
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+firebase_json = os.getenv("FIREBASE_CRED")
+cred_dict = json.loads(firebase_json)
+
+cred = credentials.Certificate(cred_dict)
+firebase_admin.initialize_app(cred)
+
+buffer = []
+
+def save_batch():
+    global buffer
+    while True:
+        time.sleep(60)  # save every 60 seconds
+
+        if len(buffer) == 0:
+            continue
+
+        data_to_save = buffer.copy()
+        buffer.clear()
+
+        doc_ref = db.collection("sensors").document()
+        doc_ref.set({
+            "timestamp": datetime.now().isoformat(),
+            "data": data_to_save
+        })
+
+        print(f"Saved batch: {len(data_to_save)} records")
+
+threading.Thread(target=save_batch, daemon=True).start()
+
+BROKER_URL = os.getenv("MQTT_BROKER")
+USERNAME = os.getenv("MQTT_USERNAME")
+PASSWORD = os.getenv("MQTT_PASSWORD")
+TOPIC = "test/inbound"
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with code:", rc)
+    client.subscribe(TOPIC)
+
+def on_message(client, userdata, msg):
+    try:
+        payload = json.loads(msg.payload.decode())
+        payload["received_at"] = datetime.now().isoformat()
+
+        print("Received:", payload)
+        buffer.append(payload)
+
+    except Exception as e:
+        print("Error:", e)
+
+client = mqtt.Client()
+client.username_pw_set(USERNAME, PASSWORD)
+client.tls_set() 
+client.on_connect = on_connect
+client.on_message = on_message
+
+print("Connecting to MQTT...")
+client.connect(BROKER_URL, 8883)
+client.loop_forever()
 
 app = Flask(__name__)
 flask_cors.CORS(app)
@@ -36,7 +103,6 @@ def predict():
     return jsonify({
         "prediction": 1 if pred == 1 else -1
     })
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
 
