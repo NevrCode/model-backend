ff73bcb69a321673847ebf17a350298fdbb37c6e
NevrCode <kevin12keval@gmail.com>
2025-11-19 12:47:38 +0700

    remove double init firebase

--- fcm_function.py
+++ fcm_function.py
@@ -1,14 +1,7 @@
 import json
 import os
 import firebase_admin
-from firebase_admin import credentials, messaging
-
-
-firebase_json = os.getenv("FIREBASE_CRED")
-cred_dict = json.loads(firebase_json)
-
-cred = credentials.Certificate(cred_dict)
-firebase_admin.initialize_app(cred)
+from firebase_admin import messaging
 
 def send_anomaly_notification(device_id, current_value):
     message = messaging.Message(
