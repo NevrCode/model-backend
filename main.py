74a2457671305fcc1031002df913487c306a8647
NevrCode <kevin12keval@gmail.com>
2025-12-05 11:40:36 +0700

    on the app

--- main.py
+++ main.py
@@ -127,7 +127,7 @@ def test_fcm():
 if __name__ == '__main__':
     threading.Thread(target=prediction_loop, daemon=True).start()
     threading.Thread(target=client.loop_forever, daemon=True).start()
-    # app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 
